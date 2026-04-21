61a591bc8b3299c2821f68bfb0f1819ddb11215b
Pratik Sawant <119058935+pratik9892@users.noreply.github.com>
2026-04-21 14:23:31 +0530

    feat:fix cookie error

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -102,7 +102,8 @@ export const login = async (req, res) => {
         // Set token in HTTP-only cookie
         res.cookie("token", token, {
             httpOnly: true,
-            sameSite: "strict",
+            secure : true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
