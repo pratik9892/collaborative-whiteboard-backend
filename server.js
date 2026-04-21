17277cbec70da3299d22a2f54783f827514cebfb
Pratik Sawant <119058935+pratik9892@users.noreply.github.com>
2026-04-21 13:37:27 +0530

    feat: add cors origin

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://collaborative-whiteboard-frontend-9qpj2tva2.vercel.app",
+    origin: "https://collaborative-whiteboard-frontend-opal.vercel.app",
     methods: ['GET', 'POST'],
     credentials:true
   }
